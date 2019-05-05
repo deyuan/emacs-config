7a8a6727e04313bd3daf34ab132a2da93a005ea9
Deyuan <guodeyuan@gmail.com>
2019-05-04 19:32:01 -0700

    Update more settings.

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;; Deyuan Notes
+;; Notes
 ;; C-w, M-w, C-y : cut, copy, paste
 ;; C-g : keyboard-quit : quit any operation
 ;; C-q <Tab> : quote-insert : inset a tab character
@@ -14,70 +14,97 @@
 ;; <Tab> : indent-for-tab-command : fix indentation of region
 ;; M-x find-file-at-point : vim's gf
 ;; M-x chmod : chmod
-;; M-x dirs : refresh current directory in shell
+;; M-x dirs, M-RET : refresh current directory in shell
 ;; M-x eval-buffer, M-x load-file : reload .emacs
 ;; M-x whitespace-mode : show whitespaces
 ;; M-x list-packages : list available packages
 ;; M-x package-install : install a package
+;; M-x ff-find-other-file : switch between source and header
+;; M-x find-tag : ctags
+;; M-x occur : show occurrence
+;; M-x list-colors-display : list all colors
 
 (package-initialize)
 
 ;; Use MELPA package repository
 (add-to-list 'package-archives '("MELPA" . "https://melpa.org/packages/") t)
 
+;; Display and Theme
+(progn
+  ;; Frame
+  (setq use-file-dialog nil)
+  (setq use-dialog-box nil)
+  (setq inhibit-startup-screen t)
+  (tool-bar-mode -1)
+  ;; (menu-bar-mode -1)
+  (scroll-bar-mode -1)
+  (setq window-divider-default-right-width 1)
+  (window-divider-mode)
+  (setq frame-title-format '((:eval (if (buffer-file-name) (abbreviate-file-name (buffer-file-name)) "%b"))))
+  ;; Theme
+  (require 'dracula-theme)
+  (load-theme 'dracula t)
+  ;; Highlight active mode line
+  (set-face-attribute 'mode-line nil :background "purple4")
+  (set-face-attribute 'mode-line-inactive nil :background "gray25")
+  (set-face-attribute 'window-divider nil :foreground "gray60")
+  )
+
 ;; Common
 (progn
-  ;; Hide startup screen
-  (setq inhibit-startup-screen t)
-  ;; Hide tool bar
-  (tool-bar-mode -1)
   ;; Put backup files in a central place
   (setq backup-directory-alist `(("." . "~/.emacs.save")))
-  ;; Require new line at the end of file
-  (setq require-final-newline t)
   ;; Unset C-z, C-x f
   (global-set-key (kbd "C-z") nil)
   (global-set-key (kbd "C-x f") nil)
+  (global-set-key (kbd "C-x C-b") nil)
+  (global-set-key (kbd "C-x C-k") nil)
   ;; Use y/n instead of yes/no
   (defalias 'yes-or-no-p 'y-or-n-p)
+  ;; Scroll smoothly
+  (setq scroll-conservatively 200)
+  (setq scroll-margin 5)
+  (add-hook 'shell-mode-hook (lambda () (make-local-variable 'scroll-margin) (setq scroll-margin 0)))
+  ;; (setq scroll-preserve-screen-position 'always)
+  ;; Mouse and touch pad
+  (setq mouse-wheel-scroll-amount '(2 ((shift) . 1) ((control) . nil)))
+  (setq mouse-wheel-progressive-speed nil)
+  (setq mouse-yank-at-point t)
   ;; Reload files if changed on disk
   (global-auto-revert-mode t)
+  ;; Show column number
+  (setq column-number-mode t)
+  ;; Tabs: width and expand
+  (setq-default tab-width 4)
+  (setq-default indent-tabs-mode nil)
+  ;; Require new line at the end of file
+  (setq require-final-newline t)
   ;; Parentheses
   (show-paren-mode t)
   ;; Type to overwrite highlighted region
   (delete-selection-mode 1)
-  ;; Scroll smoothly
-  (setq scroll-conservatively 200)
-  (setq scroll-margin 5)
-  ;; Mouse and touch pad
-  (setq mouse-wheel-scroll-amount '(1 ((shift) . 1) ((control) . nil)))
-  (setq mouse-wheel-progressive-speed nil)
-  ;; Tabs: width and expand
-  (setq-default tab-width 4)
-  (setq-default indent-tabs-mode nil)
-  ;; Whitespace: show tabs and trailing spaces in C++
-  (setq whitespace-style '(face tabs trailing tab-mark))
-  (add-hook 'c++-mode-hook (lambda () (whitespace-mode t)))
   ;; Shell: make prompts read only
   (setq comint-prompt-read-only t)
   (setq comint-scroll-to-bottom-on-input t)
   )
 
-;; Theme
+;; Whitespace: show tabs and trailing spaces in some modes
 (progn
-  (require 'dracula-theme)
-  (load-theme 'dracula t)
+  (setq whitespace-style '(face tabs trailing tab-mark))
+  (dolist (hook '(prog-mode-hook text-mode-hook conf-mode-hook))
+    (add-hook hook (lambda () (whitespace-mode t))))
   )
 
 ;; Window numbering: M-1/2/3
 (progn
-  (require 'window-numbering)
-  (custom-set-faces '(window-numbering-face ((t (:foreground "cyan1" :weight bold)))))
-  (window-numbering-mode t)
+  ;; (require 'window-numbering)
+  ;; (custom-set-faces '(window-numbering-face ((t (:foreground "cyan1" :weight bold)))))
+  ;; (window-numbering-mode t)
   )
 
 ;; Window move: Meta + Arrow
 (progn
+  (require 'windmove)
   (windmove-default-keybindings 'meta)
   )
 
@@ -90,10 +117,56 @@
   (icomplete-mode 1)
   )
 
-;; Which key
+;; Search
+(progn
+  (require 'isearch)
+  (define-key isearch-mode-map [remap isearch-delete-char] 'isearch-del-char)
+  (require 'anzu)
+  (global-anzu-mode)
+  (setq anzu-mode-lighter "")
+  )
+
+;; Which Key
 (progn
   (require 'which-key)
-  (setq which-key-idle-delay 1.0)
+  (setq which-key-idle-delay 2.0)
+  (setq which-key-lighter "")
   (which-key-mode)
   )
+
+;; Smex
+(progn
+  (require 'smex)
+  (setq-default smex-save-file (expand-file-name ".smex-items" user-emacs-directory))
+  (global-set-key [remap execute-extended-command] 'smex)
+  )
+
+;; Ace Jump
+(progn
+  (require 'ace-jump-mode)
+  (define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Aliases for multiple shells
+(defun my-switch-to-or-create-shell (s)
+  "Switch to or create a shell with buffer name s"
+  (if (get-buffer s) (switch-to-buffer s) (shell s)))
+(defun shell2 ()
+  "Switch to or create *shell*<2>"
+  (interactive)
+  (my-switch-to-or-create-shell "*shell*<2>"))
+(defun shell3 ()
+  "Switch to or create *shell*<3>"
+  (interactive)
+  (my-switch-to-or-create-shell "*shell*<3>"))
+(defun shell4 ()
+  "Switch to or create *shell*<4>"
+  (interactive)
+  (my-switch-to-or-create-shell "*shell*<4>"))
+(defun shell5 ()
+  "Switch to or create *shell*<5>"
+  (interactive)
+  (my-switch-to-or-create-shell "*shell*<5>"))
 
